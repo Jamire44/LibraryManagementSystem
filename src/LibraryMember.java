92ebd4dd4488bb597748327d0e4cdd372df62e0c
Jamire44 <2003doyle@gmail.com>
2024-02-07 21:43:50 +0000

    setup each class

--- src/LibraryMember.java
+++ src/LibraryMember.java
@@ -1,3 +1,56 @@
 public class LibraryMember {
 
+    private String name;
+    private String contactInfo;
+    private String address;
+    private int membershipID;
+    private int age;
+
+    public LibraryMember(String name, String contactInfo, String address, int membershipID, int age){
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.address = address;
+        this.membershipID = membershipID;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMembershipID() {
+        return membershipID;
+    }
+
+    public void setMembershipID(int membershipID) {
+        this.membershipID = membershipID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }//class
--- src/LibraryStaff.java
+++ src/LibraryStaff.java
@@ -1,3 +1,46 @@
 public class LibraryStaff {
 
+    private String name;
+    private String position;
+    private String contactInfo;
+    private int staffID;
+
+    public LibraryStaff(String name, String position, String contactInfo, int staffID){
+        this.name = name;
+        this.position = position;
+        this.contactInfo = contactInfo;
+        this.staffID = staffID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public int getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
 }//class
